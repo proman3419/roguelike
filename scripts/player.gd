40b6208980fa9a99a07c3aca3619d21f3dfd8ff8
proman3419 <proman7268@gmail.com>
2024-03-18 11:42:22 +0100

    Player movement

---
+++ scripts/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+var direction : Vector2 = Vector2()
+var speed = 200
+var friction = 0.1
+var acceleration = 0.1
+
+func get_input():
+	var input = Vector2()
+	if Input.is_action_pressed('right'):
+		input.x += 1
+	if Input.is_action_pressed('left'):
+		input.x -= 1
+	if Input.is_action_pressed('down'):
+		input.y += 1
+	if Input.is_action_pressed('up'):
+		input.y -= 1
+	return input
+
+func _physics_process(delta):
+	var direction = get_input()
+	if direction.length() > 0:
+		velocity = velocity.lerp(direction.normalized() * speed, acceleration)
+	else:
+		velocity = velocity.lerp(Vector2.ZERO, friction)
+	move_and_slide()
